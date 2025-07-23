8ba21bba04af4926ee50c53210f20d8dd06d6ad7
Axelotl_30 <axelgirard7@laposte.net>
2025-07-23 17:22:34 +0200

    bouncy popup

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('animate');
+      // Animate with GSAP
+      gsap.to(entry.target, {
+        duration: 0.65,
+        opacity: 1,
+        scale: 1,
+        ease: "bounce.out"
+      });
+
+      observer.unobserve(entry.target); // stop observing after animation
     }
   });
 }, {
-  threshold: 0.25 // au moins 10% visible
+  threshold: 0.4
 });
 
 document.querySelectorAll('.popup').forEach(el => observer.observe(el));
