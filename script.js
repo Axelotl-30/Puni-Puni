091eb5b1a8d04e02ff92f0feecc0125cd28847e5
Axelotl-30 <axelgirad7@laposte.net>
2025-07-31 19:28:23 +0200

    new character slider

--- script.js
+++ script.js
@@ -29,115 +29,26 @@ window.addEventListener('load', () => {
   preloader.style.display = 'none';
 });
 
-function resizeImagesToViewport() {
-  const viewportWidth = window.innerWidth;
-  const widthInPixels = 0.6 * viewportWidth;
-  const heightInPixels = widthInPixels * 2560/1440;
-  
-  document.querySelectorAll('.img-comp-img img').forEach(img => {
-    img.setAttribute('width',  widthInPixels);
-    img.removeAttribute('height', heightInPixels); // facultatif : évite déformation si ratio est important
+
+const Lcharacters = ["assets/popups/Slide_Komami.png","assets/popups/Slide_Mika.png","assets/popups/Slide_Nyarthur.png","assets/popups/Slide_Ryuta.png","assets/popups/Slide_WyvernKing.png"];
+let characterIndex = 0;
+
+function switch_character(side){
+  if (side === "left") {
+    characterIndex = (characterIndex - 1 + Lcharacters.length) % Lcharacters.length;
+  }else if (side === "right") {
+    characterIndex = (characterIndex + 1) % Lcharacters.length;
+  }else {return;} // Invalid side
+
+  const character = document.querySelector('.character');
+  character.style.backgroundImage = `url('${Lcharacters[characterIndex]}')`;
+  character.style.opacity = 0; // Reset opacity for fade-in effect
+  character.style.backgroundSize = '25vw auto'; // Reset scale for bounce effect
+  gsap.to(character, {
+    duration: 0.65,
+    opacity: 1,
+    backgroundSize: '60vw auto',
+    rotate: 0,
+    ease: "bounce.out"
   });
-
-  document.querySelectorAll('.img-comp-container').forEach(container => {
-    
-    container.style.height = `${heightInPixels}px`;
-  });
-
-  document.querySelectorAll('.img-comp-slider').forEach(el => el.remove());
-
-  initComparisons();
-
-  document.querySelectorAll('.img-comp-slider').forEach(slider => {
-    slider.style.top = `${heightInPixels/2-20}px`;
-    slider.style.left = `${widthInPixels/2-20}px`
-  });
-
-  
 }
-
-function initComparisons() {
-    var x, i;
-    /*find all elements with an "overlay" class:*/
-    x = document.getElementsByClassName("img-comp-overlay");
-    for (i = 0; i < x.length; i++) {
-        /*once for each "overlay" element:
-        pass the "overlay" element as a parameter when executing the compareImages function:*/
-        compareImages(x[i]);
-    }
-    function compareImages(img) {
-        var slider, img, clicked = 0, w, h;
-        /*get the width and height of the img element*/
-        w = img.offsetWidth;
-        h = img.offsetHeight;
-        /*set the width of the img element to 50%:*/
-        img.style.width = (w / 2) + "px";
-        /*create slider:*/
-        slider = document.createElement("DIV");
-        slider.setAttribute("class", "img-comp-slider");
-        /*insert slider*/
-        img.parentElement.insertBefore(slider, img);
-        /*position the slider in the middle:*/
-        // slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px"; ici c'est activé de base
-        // slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px"; ici c'est activé de base
-        /*execute a function when the mouse button is pressed:*/
-        slider.addEventListener("mousedown", slideReady);
-        /*and another function when the mouse button is released:*/
-        window.addEventListener("mouseup", slideFinish);
-        /*or touched (for touch screens:*/
-        slider.addEventListener("touchstart", slideReady);
-        /*and released (for touch screens:*/
-        window.addEventListener("touchend", slideFinish);
-        function slideReady(e) {
-        /*prevent any other actions that may occur when moving over the image:*/
-        e.preventDefault();
-        /*the slider is now clicked and ready to move:*/
-        clicked = 1;
-        /*execute a function when the slider is moved:*/
-        window.addEventListener("mousemove", slideMove);
-        window.addEventListener("touchmove", slideMove);
-        }
-        function slideFinish() {
-        /*the slider is no longer clicked:*/
-        clicked = 0;
-        }
-        function slideMove(e) {
-        var pos;
-        /*if the slider is no longer clicked, exit this function:*/
-        if (clicked == 0) return false;
-        /*get the cursor's x position:*/
-        pos = getCursorPos(e)
-        /*prevent the slider from being positioned outside the image:*/
-        if (pos < 0) pos = 0;
-        if (pos > w) pos = w;
-        /*execute a function that will resize the overlay image according to the cursor:*/
-        slide(pos);
-        }
-        function getCursorPos(e) {
-        var a, x = 0;
-        e = (e.changedTouches) ? e.changedTouches[0] : e;
-        /*get the x positions of the image:*/
-        a = img.getBoundingClientRect();
-        /*calculate the cursor's x coordinate, relative to the image:*/
-        x = e.pageX - a.left;
-        /*consider any page scrolling:*/
-        x = x - window.pageXOffset;
-        return x;
-        }
-        function slide(x) {
-        /*resize the image:*/
-        img.style.width = x + "px";
-        /*position the slider:*/
-        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
-        }
-    }
-    }
-
-// Exécute une première fois
-resizeImagesToViewport();
-
-// Et à chaque redimensionnement
-window.addEventListener('resize', resizeImagesToViewport);
-
-    
-  
