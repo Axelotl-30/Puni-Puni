fd5ce5857f2968d1707dc63bfb6ffb9b617490dc
Axelotl_30 <axelgirard7@laposte.net>
2025-08-26 23:23:04 +0200

    scroll upgrade

--- install.js
+++ install.js
@@ -1,4 +1,26 @@
 window.addEventListener('load', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // défilement doux
+  });
   const preloader = document.getElementById('preloader');
   preloader.style.display = 'none';
 });
+
+ const btn = document.getElementById("btnTop");
+
+    // Affiche le bouton quand on scrolle vers le bas
+    window.onscroll = function() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    };
+
+function scrollToTop() {
+  window.scrollTo({
+    top: window.innerHeight*0.25,
+    behavior: "smooth" // défilement doux
+  });
+}
--- script.js
+++ script.js
@@ -24,6 +24,10 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.popup').forEach(el => observer.observe(el));
 
 window.addEventListener('load', () => {
+  window.scrollTo({
+    top: window.innerHeight*0.25,
+    behavior: "smooth" // défilement doux
+  });
   const preloader = document.getElementById('preloader');
   preloader.style.display = 'none';
 });
@@ -85,4 +89,22 @@ function switch_character(side){
     });
   }
   
+}
+
+ const btn = document.getElementById("btnTop");
+
+    // Affiche le bouton quand on scrolle vers le bas
+    window.onscroll = function() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    };
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // défilement doux
+  });
 }
